4bbb01449902837d83090b200df5dc862bfdf937
loic cessot <loic.cessot@gadz.org>
2017-05-03 15:53:56 +0200

    rails new

--- app/controllers/coachings_controller.rb
+++ app/controllers/coachings_controller.rb
@@ -1,7 +1,15 @@
 class CoachingsController < ApplicationController
+
   def answer
+     @question = params[:question]
+     @date = Time.now
+     @answer = "Ta question est stupide par définition"
   end
 
   def ask
   end
+
+  def home
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/answer', to: 'coachings#answer'
-
-  get '/ask', to: 'coachings#ask'
+  get '/ask' => 'coachings#ask'
+  root 'coachings#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
